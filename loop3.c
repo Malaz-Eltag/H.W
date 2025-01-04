f17b6a48f619cd049afff67f78a41c1df9a1cd41
Malaz-Elatag <malazaltag123@gmail.com>
2025-01-04 23:39:58 +0200

    commit

---
+++ loop3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main () {  int m=0;
+    do {
+    
+        printf("#");
+
+        m++;
+    }while( m  < 10);
+        printf("\n");
+
+    for (int x=1;x<6;x++) {
+ 
+        for(int y=1;y<2;y++){
+            printf("#"); }
+
+        for (int y=1;y<9;y++){
+        
+            printf("*");}
+        for(int o=10;o>9;o--){
+            printf("#"); }
+
+    printf("\n");}
+
+int u =0;
+do {
+    
+    printf("#");
+
+        u++;
+    }while( u < 10);
+     
+
+    return(0);
+}
