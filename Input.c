d1c860342be2ea3a7a2a35a0f04e99769297071c
Malaz-Elatag <malazaltag123@gmail.com>
2025-01-08 23:18:47 +0200

    Functions

--- Input.c
+++ Input.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-int Name(char str[])
- { int u=0;
- while (str[u]!= '\0'){
-printf("%c",str[u]);
-u++;
- }
-    printf("\n");
-
- }
-
-
 int main() {
-    char my[] = "malaz eltag";
-   printf("Your First Name is: ");
-   Name(my);
-
 /*char u[12] ;
 printf("Enter Your Name :");
 scanf("%s",u);
 printf("Your Name is: %s\n",u);
-
+*/
 
 char Name[15];
+int Age;
+int c;
 printf("Enter Your Name :");
-fgets( Name ,sizeof(Name),stdin );
+fgets( Name ,sizeof(Name),stdin);
+/*while(Name!=  ) {
+    printf(" please Enter Number !");
+    return(1);
+}
+*/
+
 //printf("\n");
-printf("Your Name is: %s", Name);
-printf("Hello, %s", Name);
-//printf("\n");*/
+printf("Enter Your Age :" );
+scanf("%d" ,&Age);
+
+
+printf("Your Name is: %sand Your Age is: %d ",Name,Age );
+//printf("Hello, %s", Name);
+//printf("\n");
+
 
     return(0);
 }
