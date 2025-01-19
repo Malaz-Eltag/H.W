f9677100a42ab4635e48608d9b5920da4d70fa6e
Malaz-Elatag <malazaltag123@gmail.com>
2025-01-19 17:00:22 +0200

    course

--- python/H.W 3.py
+++ python/H.W 3.py
@@ -1,11 +1,8 @@
-
 list =[ 1 , "Sahar" , 1.7 , 2 , 5.3, "Fatima" ,23.4  ,"Wala" ,8.8, "Mona" ,2.8 , 5 ,"Mina"]
 p = 0
 for i in (list)  :
-    if isinstance(i, str):
-        print(f"That's not a number : {i}")
+    if isinstance(i, str) :
+        print(f"This is not a number : {i}")
     if isinstance(i, int ) or isinstance(i, float ):
         p+= i
-print(f"The sum of the numbers  : {p}")
- 
-    
+print(f"The sum of the numbers is : {p}")
